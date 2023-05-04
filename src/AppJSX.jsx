f46db08b23769e906513d508fb6c55c103a7c66c
sonyj <sonyj@eseict.com>
2023-05-04 18:04:31 +0900

    test - 집에서 하려고 임시 커밋

---
+++ src/AppJSX.jsx
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+function AppJSX() {
+
+  const name = 'son';
+  const list = ['우유', '딸기', '바나나'];
+  return (
+    <>
+      <h1 className='orange'>{`hello ${name}`}</h1>
+      <p>{name}</p>
+
+      <ul>
+        {
+          list.map((item) => {
+            return <li>{item}</li>
+          })
+        }
+      </ul>
+
+    </>
+    
+    // <div className="AppJSX">
+    //   <header className="AppJSX-header">
+    //     <img src={logo} className="AppJSX-logo" alt="logo" />
+    //     <p>
+    //       Edit dsf <code>src/AppJSX.jsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="AppJSX-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default AppJSX;
